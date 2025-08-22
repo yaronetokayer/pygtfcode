54be5d8bcc65fcde6aee912a034549103d8a413b
yaronetokayer <109242322+yaronetokayer@users.noreply.github.com>
2025-08-21 23:40:12 -0500

    cleanup

--- pygtfcode/evolve/hydrostatic.py
+++ pygtfcode/evolve/hydrostatic.py
@@ -104,7 +104,7 @@ def _update_r_p_rho_v2(r, x, p, rho):
     tiny = np.finfo(np.float64).tiny
     V_new = np.maximum(V_new, tiny)
 
-    ratio = V_old / V_new  # = V / V'  (exact)
+    ratio = V_old / V_new
     gamma = 5.0 / 3.0
 
     rho_new = rho * ratio
@@ -132,42 +132,6 @@ def _update_r_p_rho_v2(r, x, p, rho):
     
 #     return r_new, p_new, rho_new, v2_new
 
-# @njit(types.Tuple((float64[:], float64[:], float64[:], float64[:]))
-#       (float64[:], float64[:], float64[:], float64[:]), cache=True, fastmath=True)
-# def _update_r_p_rho_v2(r, x, p, rho) -> tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
-#     # Old method from Frank's (potentially mistaken) implementation
-#     # r_new = r.copy()
-#     # r_new[1:-1] *= (1.0 + x)
-#     # r3c = r[1:]**3 / (r[1:]**3 - r_new[:-1]**3)
-
-#     rL = r[:-1]
-#     rR = r[1:]
-#     rR3 = rR**3
-#     r3c = rR3 / (rR3 - rL**3)
-#     r3c_minus1 = r3c - 1.0
-
-#     dV_over_V = np.empty(r3c.shape, dtype=np.float64)
-#     dV_over_V[0]    = 3.0 * r3c[0] * x[0]
-#     dV_over_V[1:-1] = 3.0 * (r3c[1:-1] * x[1:] - r3c_minus1[1:-1] * x[:-1])
-#     dV_over_V[-1]   = -3.0 * r3c_minus1[-1] * x[-1]
-
-#     p_new   = p   * (1.0 - (5.0 / 3.0) * dV_over_V)
-#     rho_new = rho * (1.0 - dV_over_V)
-#     v2_new  = p_new / rho_new
-
-#     # build r_new without copying r
-#     r_new = np.empty(r.shape, dtype=np.float64)
-#     r_new[0]  = r[0]
-#     r_new[-1] = r[-1]
-#     r_new[1:-1] = r[1:-1] * (1.0 + x)
-
-#     return r_new, p_new, rho_new, v2_new
-
-# @njit(
-#     types.Tuple((float64[:, :], float64[:]))   # returns (ab, y)
-#     (float64[:], float64[:], float64[:], float64[:]),  # args: r, rho, p, m
-#     cache=True
-# )
 @njit(types.Tuple((float64[:], float64[:], float64[:], float64[:]))
       (float64[:], float64[:], float64[:], float64[:]), cache=True, fastmath=True)
 def build_tridiag_system(r, rho, p, m_tot) -> tuple[np.ndarray, np.ndarray]:
@@ -232,14 +196,7 @@ def build_tridiag_system(r, rho, p, m_tot) -> tuple[np.ndarray, np.ndarray]:
     b = -2.0 - r3b * c1 - r3c * c2                  # Main diagonal, except first element
     c = r3a * c1 + q1                               # Superdiagonal
 
-    # Banded matrix format for solve_banded
-    # ab = np.zeros((3, (r.size - 2)), dtype=np.float64)
-    # ab[0, 1:] = (r3a * c1 + q1)[:-1]               # Superdiagonal
-    # ab[1, :]  = -2.0 - r3b * c1 - r3c * c2         # Main diagonal
-    # ab[2, :-1] = (r3d * c2 - q2)[1:]               # Subdiagonal
-
     return a, b, c, y
-    # return ab, y
 
 @njit(float64[:](float64[:], float64[:], float64[:], float64[:]), cache=True, fastmath=True)
 def solve_tridiagonal_frank(a, b, c, y):
